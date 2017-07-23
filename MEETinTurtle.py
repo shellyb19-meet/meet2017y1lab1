03e5d3f4b951ddad3714558e42cd5a8f753158bd
shellyb19-meet <shellyb19@meet.mit.edu>
2017-07-23 19:05:42 +0300

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,194 @@
+import turtle
+
+# Everything that comes after the # is a
+
+# comment.
+
+# It is a note to the person reading the code.
+
+# The computer ignores it.
+
+# Write your code below here...
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100) #Move the turtle to the
+
+#position (-200, -100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+#Draw the M:
+
+turtle.goto(-200,-100+200)
+
+turtle.goto(-200+50,-100)
+
+turtle.goto(-200+100,-100+200)
+
+turtle.goto(-200+100,-100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100) #Move the turtle to the
+
+#position (-200, -100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+#Draw the E:
+
+turtle.goto(-200,-100+200)
+
+turtle.goto(-200+50,100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100+100) #Move the turtle to the
+
+#position (-200, -100+100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+turtle.goto(-200+50,-100+100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100) #Move the turtle to the
+
+#position (-200, -100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+turtle.goto(-200+50,-100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100) #Move the turtle to the
+
+#position (-200, -100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+#Draw the E:
+
+turtle.goto(-200,-100+200)
+
+turtle.goto(-200+50,100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100+100) #Move the turtle to the
+
+#position (-200, -100+100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+turtle.goto(-200+50,-100+100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100) #Move the turtle to the
+
+#position (-200, -100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+turtle.goto(-200+50,-100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100) #Move the turtle to the
+
+#position (-200, -100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+#Draw the E:
+
+turtle.goto(-200,-100+200)
+
+turtle.goto(-200+50,100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100+100) #Move the turtle to the
+
+#position (-200, -100+100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+turtle.goto(-200+50,-100+100)
+
+turtle.penup() #Pick up the pen so it doesn’t
+
+#draw
+
+turtle.goto(-200,-100) #Move the turtle to the
+
+#position (-200, -100)
+
+#on the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+turtle.goto(-200+50,-100)
+
+# ...and end it before the next line.
+
+turtle.mainloop()
